0eb86c330d312abe8ab4e8e5f0c6cb3fbca641d2
kenzaES <kenza.esdiri@gmail.com>
2024-04-15 15:33:58 +0100

    modified:   Blog_code.html 	deleted:    background-opacity.png 	modified:   blog_java.js 	modified:   bolg_style.css 	modified:   image/caftan story.jpg 	modified:   image/couscous.jpg 	new file:   image/footer_image.jpg 	new file:   image/footer_image2.jpg 	new file:   image/footer_op.png 	new file:   image/princess caftan.jpg 	renamed:    account-svgrepo-com.svg -> image/signIn_active.svg 	renamed:    account-svgrepo-com (1).svg -> image/signIn_innactive.svg 	deleted:    logoblog.svg 	deleted:    maroc-traditions.jpg 	new file:   post_page_trend1.html

--- blog_java.js
+++ blog_java.js
@@ -1,2 +1,20 @@
-let myHeading = document.querySelector("h1");
-myHeading.textContent = "Bonjour, monde !";
+clickCounter();
+
+function clickCounter() {
+  if (localStorage.clickcount) {
+    localStorage.clickcount = Number(localStorage.clickcount)+1;
+  } else {
+    localStorage.clickcount = 1;
+  }
+  document.getElementById("demo").innerHTML = localStorage.clickcount;
+} 
+
+function myFunction(){
+    let data = "";  let name = document.getElementById("userName").value
+    let email = document.getElementById("userEmail").value
+    let comment = document.getElementById("userComment").value
+             
+    data = "User name : "+name+"<br/>User email : "+email+ "<br/>User comment : "+comment
+             
+            document.getElementById("data").innerHTML = data  // display data to paragraph
+            }
